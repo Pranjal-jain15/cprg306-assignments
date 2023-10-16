ae11b61354457c17bd737162a68cd45a020565c2
Pranjal-jain15 <141540534+Pranjal-jain15@users.noreply.github.com>
2023-10-16 16:17:43 -0600

    Week5 Done

--- app/page.js
+++ app/page.js
@@ -7,8 +7,9 @@ export default function week2() {
       <h1 style={{ fontSize:45}}> CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
       <Link href="./week2">Week2</Link><br></br>
-      <Link href="./week3"> Week3</Link><br></br>
-      <Link href="./week4/">Week4</Link> <br></br>
+      <Link href="./week3">Week3</Link><br></br>
+      <Link href="./week4">Week4</Link> <br></br>
+      <Link href="./week5">Week5</Link> <br></br>
     </main>
    
   )
---
+++ app/week5/page.js
@@ -0,0 +1,15 @@
+"use client";
+import Link from 'next/link'
+import ItemList from "./item-list"
+
+export default function Page() {
+  return (
+    <main>
+      <h1 style={{ fontSize:45}}>My Shopping List</h1>
+      <ItemList/>
+
+      <Link href="../">Click here to go to main page</Link>
+    </main>
+   
+  )
+}
