c57e1a510d9a9e185eccc7bdb6dd28ff464df665
Pranjal-jain15 <141540534+Pranjal-jain15@users.noreply.github.com>
2023-11-06 20:00:49 -0700

    -

--- app/week8/page.js
+++ app/week8/page.js
@@ -1,18 +1,33 @@
 "use client"
+
+import React from "react";
 import { useUserAuth } from "./_utils/auth-context";
- export default function Page(){
+
+
+const Page = () => {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
-  function handleSignIn() {
-    gitHubSignIn();
-  }
-  function handleSignOut(){
-    firebaseSignOut();
-  }
+  const handleLogin = async () => {
+    await gitHubSignIn();
+  };
+
+  const handleLogout = async () => {
+    await firebaseSignOut();
+  };
+
   return (
-   
+    <div>
+      {user ? (
+        <div>
+          <p>Welcome, {user.displayName} ({user.email})</p><br></br>
+          <button onClick={handleLogout}>Logout</button><br></br>
+          <a href="./week8/shopping-list">Click here to Access shopping list</a> <br></br>
+        </div>
+      ) : (
+        <button onClick={handleLogin}>Login with GitHub</button>
+      )}
+    </div>
+  );
+};
 
-
-  )
- }
-
+export default Page;
